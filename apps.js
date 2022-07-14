5e78c30ca53209e2a9ac05a313c9e14df9b1a9b5
Jimbei <contrerasr2272@gmail.com>
2022-07-14 09:16:40 -0400

    adding questions to readme

--- apps.js
+++ apps.js
@@ -1,17 +1,37 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const name = profileDataArgs[0];
 const inquirer = require('inquirer');
+/*const fs = require('fs');
+const generateReadMe = require('./src/page-template.js');
+const pageREADME = generateReadMe(Name);
+
 const writeFile = require('./potential-enigma/Develop/utils/generate-site.js');
 // TODO: Create an array of questions for user input
 // TODO: Create a function to write README file
 //function writeToFile(fileName, data) {}
 
 
+//questions();
 
 
+fs.writeFile('README.md', pageREADME(Name), err => {
+    if (err) throw err;
 
+    console.log('Readme file complete!')
+    //console.log(generateReadMe(name));
+        
+    
+});
+*/
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the title of your project?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const questions = () => {
+/*const questions = () => {
     return inquirer.prompt ([
        {
         type: 'input',
@@ -29,37 +49,8 @@ const questions = () => {
        } 
     ]);
 };
-
-const generatePage2 = (userName) => {
-    return `# <Your-Project-Title>
-
-    ## Description
-    
-    Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-    
-    - What was your motivation?:
-    - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-    - What problem does it solve?: 
-    - What did you learn?
-    
-    ## Table of Contents (Optional)
-    
-    
-    Name: ${userName}`;
-} 
-console.log(generatePage2(name));
-
-
-questions();
-
-
-/*fs.writeFile('README.md',questions(), err => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-});
-
-
-console.log(generatePage());
 */
+
+
+
+
--- src/page-template.js
+++ src/page-template.js
@@ -1,23 +1,25 @@
-const generatePage = () => {
-    return `
-# <Your-Project-Title>
+const generateReadMe = (userName) => {
+    return `# <Your-Project-Title>
 
-## Description
-
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-- What was your motivation?:
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?: 
-- What did you learn?
-
-## Table of Contents (Optional)` `Name: ricardo, Github: me
-`;
-
-
+    ## Description
+    
+    Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+    
+    - What was your motivation?: ${userName}
+    - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+    - What problem does it solve?: 
+    - What did you learn?
+    
+    ## Table of Contents (Optional)
+    
+    
+    Name: ${userName}
+    
+    `;
 };
 
 
-module.exports = {
-    generatePage: generatePage
-};
+
+
+
+module.exports = generateReadMe;
